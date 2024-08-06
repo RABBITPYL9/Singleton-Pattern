17091a7f8a872acfc347878b000ff4e189337000
Sergey <45532331+RABBITPYL9@users.noreply.github.com>
2024-08-06 15:47:24 +0300

    Create Singleton.py

---
+++ Singleton.py
@@ -0,0 +1,24 @@
+
+import psycopg2
+
+class Singleton(type):
+
+    """
+    Метакласс Singleton. Необходим для ограничения кол-ва инстансов класса (1 инстанс)
+    Пример использованиея: class DbConnection(metaclass=Singleton): 
+    """
+    _instances = {}
+
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls)\
+                .__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class DDbConnection(metaclass=Singleton):
+    def __init__(self):
+        conn1 = psycopg2.connect(dbname='singleton', user='admin', password='admin', host='localhost')
+        cursor = conn1.cursor()
+        print('Initializing singleton database connection... ')
