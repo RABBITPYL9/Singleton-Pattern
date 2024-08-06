20b0174c84da6a35d113592fff4a0d00b55d988e
Sergey <45532331+RABBITPYL9@users.noreply.github.com>
2024-08-06 17:18:21 +0300

    Update Singleton.py

--- Singleton.py
+++ Singleton.py
@@ -10,14 +10,29 @@ class Singleton(type):
     _instances = {}
 
 
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+    
+
+class Database(metaclass=Singleton):
+    connection = None
+    
+    def connect(self):
+        if self.connection is None:
+            self.connection = psycopg2.connect(dbname='statsmon', user='admin', password='upt24', host='192.168.2.197')
+            self.cursorobj = self.connection.cursor()
+            self.execute = self.cursorobj.execute("""
+                 update public.tests
+                 set status = 'single'
+                 where id = 315""")
+            self.cursorobj.execute("commit;")
+        return self.cursorobj
 
 
-class DDbConnection(metaclass=Singleton):
-    def __init__(self):
-        db_connection = psycopg2.connect(dbname='singleton', user='admin', password='admin', host='localhost')
-        cursor = db_connection.cursor()
-        print('Initializing singleton database connection... ')
+
+
+db1 = Database().connect()
+print ("Database Objects DB1", db1)
